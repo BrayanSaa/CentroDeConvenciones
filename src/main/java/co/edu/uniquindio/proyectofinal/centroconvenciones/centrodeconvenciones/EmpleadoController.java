4e64b8f92e62520f9e8cce4ba4e5a1b7e2160dad
brayanSaa <blackd213a@gmail.com>
2024-04-01 06:55:05 -0500

    Se crea la interfaz , sin el funcionamiento de manera base

---
+++ src/main/java/co/edu/uniquindio/proyectofinal/centroconvenciones/centrodeconvenciones/EmpleadoController.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.proyectofinal.centroconvenciones.centrodeconvenciones;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EmpleadoController {
+    @FXML
+    private Label welcomeText;
+
+
+    public void nuevoEmpleadoAction(ActionEvent actionEvent) {
+    }
+
+    public void agregarEmpleadoAction(ActionEvent actionEvent) {
+    }
+
+    public void eliminarEmpleadoAction(ActionEvent actionEvent) {
+    }
+
+    public void actualizarEmpleadoAction(ActionEvent actionEvent) {
+    }
+}
